5e8bceea8d75a38cdfda66aff6886208c40a4e46
samdd <sam@samdd.me>
2018-03-15 03:32:13 +0000

    v1.3.0 - MacOS specific styles

--- src/styles/chrome.jsx
+++ src/styles/chrome.jsx
@@ -3,43 +3,73 @@ import preset from 'jss-preset-default'
 
 jss.setup(preset())
 
-export default config => {
+const blur = false
+
+export default (config) => {
+  if (!config.os) {
+    config.os = 'windows'
+    if (navigator.platform.indexOf('Mac') >= 0) config.os = 'mac'
+  }
   const fontColor = config.variant === 'dark' ? 'rgba(255,255,255,0.9)' : '#000'
   const styles = {
+    '@font-face': {
+      fontFamily: '"San Francisco"',
+      fontWeight: 500,
+      src:
+        'url("https://applesocial.s3.amazonaws.com/assets/styles/fonts/sanfrancisco/sanfranciscodisplay-medium-webfont.woff2")'
+    },
     root: {
+      visibility: 'hidden',
       boxSizing: 'border-box',
       cursor: 'default',
       minWidth: '270px',
       maxWidth: '100%',
-      backgroundColor: config.variant === 'dark' ? '#505050' : '#ffffff',
+      backgroundColor:
+        config.os === 'mac'
+          ? config.variant === 'dark'
+            ? blur ? 'rgba(80, 80, 80, 0.73)' : '#7C7C7C'
+            : blur ? 'rgba(224, 228, 239, 0.73)' : '#E9E7E9'
+          : config.variant === 'dark' ? '#505050' : '#ffffff',
       border: `1px solid ${config.variant === 'dark' ? '#6f6f6f' : '#bababa'}`,
-      padding: '2px 0',
-      boxShadow: '4px 4px 4px -3px rgba(0, 0, 0, 0.75)',
+      padding: config.os === 'mac' ? '3px 0' : '2px 0',
+      boxShadow:
+        config.os === 'mac'
+          ? '0px 7px 14px rgba(0, 0, 0, 0.25)'
+          : '4px 4px 4px -3px rgba(0, 0, 0, 0.75)',
       outline: 'none',
+      transition: config.os === 'mac' ? 'opacity 0.1s ease' : '',
       userSelect: 'none',
       overflow: 'hidden',
+      borderRadius: config.os === 'mac' ? '6px' : 0,
+      backdropFilter: config.os === 'mac' && blur ? 'blur(35px)' : 'initial',
       zIndex: 999999999,
       ...config.styles.root,
       '& *': {
         boxSizing: 'border-box'
       },
+      '&.react-contextmenu--visible': {
+        visibility: 'initial'
+      },
       '& .react-contextmenu-item:not(.react-contextmenu-item--divider)': {
         display: 'flex',
         outline: '0',
-        height: '24px',
-        lineHeight: '24px',
+        height: config.os === 'mac' ? '20px' : '24px',
+        lineHeight: config.os === 'mac' ? '20px' : '24px',
         padding: '0 25px',
         color: fontColor,
-        fontFamily: `'Segoe UI'`,
-        fontSize: '12px',
+        fontFamily:
+          config.os === 'mac' ? `'San Francisco'` : `'Segoe UI', sans-serif`,
+        fontSize: config.os === 'mac' ? '14px' : '12px',
         ...config.styles.item,
         '&:hover': {
           backgroundColor:
-            config.variant === 'dark'
-              ? 'rgba(255, 255, 255, 0.1)'
-              : 'rgba(0, 0, 0, 0.07)',
+            config.os === 'mac'
+              ? '#1F7CF7'
+              : config.variant === 'dark'
+                ? 'rgba(255, 255, 255, 0.1)'
+                : 'rgba(0, 0, 0, 0.07)',
           '& *': {
-            color: fontColor
+            color: config.os === 'mac' ? '#fff' : fontColor
           }
         }
       },
@@ -50,10 +80,12 @@ export default config => {
       },
       '& .react-contextmenu-item--divider': {
         pointerEvents: 'none',
-        height: '1px',
+        height: config.os === 'mac' ? '2px' : '1px',
         background:
-          config.variant === 'dark' ? 'rgba(255,255,255,0.1)' : '#e9e9e9',
-        margin: '5px 1px',
+          config.os === 'mac'
+            ? 'rgba(0, 0, 0, 0.07)'
+            : config.variant === 'dark' ? 'rgba(255,255,255,0.1)' : '#e9e9e9',
+        margin: config.os === 'mac' ? '5px 0' : '5px 1px',
         ...config.styles.divider
       }
     },
